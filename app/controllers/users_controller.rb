22f01c1c6fdd4676f084dbcadc521326ba3562a3
phamhuycuong <phamhuycuong@framgia.com>
2015-06-10 13:38:17 +0700

    sign_up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
+class Check_user < ActiveModel::Validator
+    
+  def validate(record)
+    if nil != record.id
+      
+    else
+      if User.find_by_name(record.name)
+        record.errors[:base] << "User name have been used"
+      else
+        if User.find_by_email(record.email)
+          record.errors[:base] << "User email have been used"
+        end
+      end
+    end
+  end
+end
+
 class User < ActiveRecord::Base
   has_many :lessons, dependent: :destroy
+
+  has_secure_password
+
+  validates :name, length: {minimum:6, too_short: "must have at least %{count} words"}
+  validates :password, length: {minimum:6, too_short: "must have at least %{count} words"}
+
+  validates_with Check_user
 end
